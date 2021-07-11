6d4012fe75836c14882dea5d7495e6752a9a0e76
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-07-11 12:03:51 -0300

    init

--- app.py
+++ app.py
@@ -2,17 +2,9 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_restful import Api
 
-app = Flask(__name__, static_folder="frontend/build")
+app = Flask(__name__)
 CORS(app)
 api = Api(app)
-
-from backend.hello import HelloWorld
-
-api.add_resource(HelloWorld, "/hello")
-
-@app.route("/", defaults={"path": ''})
-def root(path):
-    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run()
