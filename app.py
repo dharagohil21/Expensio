87de3d0709ca48e07718be4dbb3e7485ee15347e
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-07-17 13:09:52 -0300

    add helloworld endpoint

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+def hello_world():
+    return {"message": "hello world"}
+
+app.add_url_rule("/", "helloworld", hello_world, methods=["GET", "POST"])
+
 if __name__ == "__main__":
     app.run()
