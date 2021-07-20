7c93eb0dc3208de8151b7de464ce246d11d5bc82
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-07-20 15:38:20 -0300

    fix expense model

--- src/expense/models.py
+++ src/expense/models.py
@@ -1,5 +1,5 @@
 from src.common.models import db
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
 from sqlalchemy.dialects.mysql import DATE
 from src.users.models import User
 
@@ -10,7 +10,7 @@ class Expense(db.Model):
     expense_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     title = Column(String(100), nullable=True)
-    amount = Column(String(100), nullable=False)
+    amount = Column(Float, nullable=False)
     expense_category = Column(String(100), nullable=False)
     is_recurring = Column(Boolean, nullable=True, default=False)
     date = Column(DATE, nullable=False)
