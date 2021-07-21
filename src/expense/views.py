a82b9d7980d17298f515c06f6db5cc0407420a3f
Jaspreet Kaur Gill <js523380@dal.ca>
2021-07-21 02:30:20 -0300

    handle recurring expense in list expense and delete expense api

--- src/expense/views.py
+++ src/expense/views.py
@@ -2,7 +2,7 @@
 Author: Sravani Pinninti, Jaspreet Kaur Gill
 """
 from datetime import date, timedelta
-from dateutil.rrule import rrule, MONTHLY
+from dateutil.relativedelta import relativedelta
 from marshmallow import ValidationError
 from flask import g
 from flask_restful import Resource, request, current_app
@@ -30,11 +30,22 @@ class ExpenseResource(AuthResource):
 
     def delete(self, expense_id):
         expense = Expense.query.get(expense_id)
-
+        current_user = g.current_user
         if not expense:
             return get_response_obj("No expenses found", error="No expense with given id"), 404
 
         try:
+            next_month_date = expense.date + relativedelta(months=1)
+            next_month_start = date(next_month_date.year, next_month_date.month, 1)
+            Expense.query.filter(
+                and_(
+                    Expense.user_id == current_user.id,
+                    Expense.date >= next_month_start,
+                    Expense.amount == expense.amount,
+                    Expense.expense_category == expense.expense_category,
+                    Expense.is_recurring == True,
+                )
+            ).delete()
             expense.delete()
         except SQLAlchemyError as e:
             current_app.logger.exception("Error deleting expense")
@@ -72,7 +83,7 @@ class ExpenseResource(AuthResource):
                 expense.is_recurring = new_expense.is_recurring
                 if new_expense.is_recurring is False:
                     # when recurring flag is changed from true to false
-                    next_month_date = rrule(freq=MONTHLY, count=2, dtstart=expense.date)[1]
+                    next_month_date = expense.date + relativedelta(months=1)
                     next_month_start = date(next_month_date.year, next_month_date.month, 1)
                     Expense.query.filter(
                         and_(
@@ -134,11 +145,11 @@ class ExpenseListResource(AuthResource):
             return get_response_obj("Invalid request params", error=e.messages), 422
 
         start_date = date(req_args["date"].year, req_args["date"].month, 1)
-        next_month_date = rrule(freq=MONTHLY, count=2, dtstart=start_date)[1]
+        next_month_date = start_date + relativedelta(months=1)
         end_date = date(next_month_date.year, next_month_date.month, 1)
         current_app.logger.info("Listing expense from %s to %s", start_date, end_date)
 
-        expenses = (
+        current_month_expenses = (
             Expense.query.filter_by(user_id=current_user.id)
             .filter(
                 and_(
@@ -148,7 +159,50 @@ class ExpenseListResource(AuthResource):
             )
             .all()
         )
+
+        if start_date >= date.today(): # if future date
+            prev_month_recurr_expenses = Expense.query.filter(
+                and_(
+                    Expense.date >= start_date + relativedelta(months=-1),
+                    Expense.date < start_date,
+                    Expense.is_recurring == True
+                )
+            ).all()
+            new_expenses = list()
+            for exp in prev_month_recurr_expenses:
+                matched_expense = next(
+                    (
+                        e for e in current_month_expenses
+                        if e.amount == exp.amount
+                        and e.expense_category == exp.expense_category
+                        and e.is_recurring is True
+                    ),
+                    None,
+                )
+                if matched_expense is not None:
+                    current_app.logger.info("Recurring expense entry found")
+                    continue
+
+                expense = Expense(
+                    user_id=current_user.id,
+                    title=exp.title,
+                    amount=exp.amount,
+                    expense_category=exp.expense_category,
+                    is_recurring=True,
+                    date=start_date,
+                )
+                new_expenses.append(expense)
+                current_app.logger.info("creating new recurring expense")
+            try:
+                session = db.session
+                session.add_all(new_expenses)
+                session.commit()
+                current_month_expenses += new_expenses
+            except SQLAlchemyError as e:
+                current_app.logger.exception("Error creating recurring expense")
+                return get_response_obj("Server error listing expense", error="Database error"), 500
+
         return (
-            get_response_obj("expense list", data=expense_schema.dump(expenses, many=True)),
+            get_response_obj("expense list", data=expense_schema.dump(current_month_expenses, many=True)),
             200,
         )
