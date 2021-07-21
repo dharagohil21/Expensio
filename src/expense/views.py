5dfe05c2424ec189c3aeca87e37299cf171f1776
Jaspreet Kaur Gill <js523380@dal.ca>
2021-07-21 00:54:06 -0300

    update expense api

--- src/expense/views.py
+++ src/expense/views.py
@@ -1,5 +1,5 @@
 """
-Author: Sravani Pinninti
+Author: Sravani Pinninti, Jaspreet Kaur Gill
 """
 from datetime import date, timedelta
 from dateutil.rrule import rrule, MONTHLY
@@ -15,7 +15,7 @@ from src.common.models import db
 from src.auth.api import AuthResource
 
 
-class ExpenseResource(Resource):
+class ExpenseResource(AuthResource):
 
     def get(self, expense_id):
         expense = Expense.query.filter_by(id=expense_id).first()
@@ -44,6 +44,54 @@ class ExpenseResource(Resource):
             ), 500
 
         return get_response_obj("Expense deleted", data=None), 200
+
+    def put(self, expense_id):
+        expense = Expense.query.filter_by(id=expense_id).first()
+        if not expense:
+            return get_response_obj("No expenses found", error="No expense with given id"), 404
+
+        expense_schema = ExpenseSchema()
+        try:
+            req_data = request.json
+            new_expense = expense_schema.load(req_data, session=db.session, partial=True)
+        except ValidationError as e:
+            current_app.logger.exception("Cannot update expense, invalid request data")
+            return get_response_obj(
+                "Cannot update expense, invalid request data",
+                error=e.messages,
+            ), 422
+
+        if "title" in req_data and new_expense.title != expense.title:
+            expense.title = new_expense.title
+        if "amount" in req_data and new_expense.amount != expense.amount:
+            expense.amount = new_expense.amount
+
+        try:
+            current_user = g.current_user
+            if "is_recurring" in req_data and new_expense.is_recurring != expense.is_recurring:
+                expense.is_recurring = new_expense.is_recurring
+                if new_expense.is_recurring is False:
+                    # when recurring flag is changed from true to false
+                    next_month_date = rrule(freq=MONTHLY, count=2, dtstart=expense.date)[1]
+                    next_month_start = date(next_month_date.year, next_month_date.month, 1)
+                    Expense.query.filter(
+                        and_(
+                            Expense.user_id == current_user.id,
+                            Expense.date >= next_month_start,
+                            Expense.amount == expense.amount,
+                            Expense.expense_category == expense.expense_category,
+                            Expense.is_recurring == True,
+                        )
+                    ).delete()
+            expense.update()
+        except SQLAlchemyError as e:
+            current_app.logger.exception("Error updating expense")
+            return get_response_obj(
+                "Server error while updating expense",
+                error="Database error"
+            ), 500
+
+        return get_response_obj("Expense updated", data=expense_schema.dump(expense)), 200
 
 
 class ExpenseListResource(AuthResource):
